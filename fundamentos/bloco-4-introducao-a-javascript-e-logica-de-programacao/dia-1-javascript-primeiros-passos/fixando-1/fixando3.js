5fc0682b23fc4f0afcd9589919c977f33b253beb
Artur <arturvl1@hotmail.com>
2022-08-18 16:08:14 -0300

    adiciona os fixandos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixando-1/fixando3.js
@@ -0,0 +1,46 @@
+// let notaCandidato = 78;
+
+// if (notaCandidato >= 80 && notaCandidato<=100){
+//     console.log('Parabéns, você foi aprovado!');
+// }
+// else if (notaCandidato<80 && notaCandidato>=60){
+//     console.log('Você está na nossa lista de espera!');
+// }
+// else {
+//     console.log('Você foi reprovado!');
+// }
+ 
+// Operador AND &&
+// const currentHour = 20;
+// let message = '';
+// if(currentHour>=22){
+//     message = 'Não deveríamos comer nada, é hora de dormir';
+
+// }
+// else if(currentHour>=18 && currentHour<22){
+//     message = 'Rango da noite, vamos jantar :D';
+// }
+// else if(currentHour>=14 && currentHour<18){
+//     message = 'Vamos fazer um bolo pro café da tarde?';
+// }
+// else if(currentHour>=11 && currentHour<14){
+//     message='Hora do almoço!!!';
+// }
+// else {
+//     message='Hmmm, cheiro de café recém passado'
+// }
+
+// console.log(message);
+
+//Operador OR || 
+
+// let weekDay ='quarta-feira';
+// if(weekDay === 'segunda-feira' || weekDay === 'terça-feira' ||weekDay === 'quarta-feira' ||weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+//     console.log ('Oba, mais um dia de aprendizado na Trybe');
+// }
+// else {
+//     console.log('FINALMENTE, descanso merecido UwU');
+// }
+
+
+// Operador NOT 
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/fixando-1/scripts.js
@@ -0,0 +1,12 @@
+let myName = 'Artur';
+let birthCity = 'Piracuruca';
+let birthYear = 1998; 
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030; 
+console.log(birthYear);
+birthCity = 'Sobral';
+console.log(birthCity);
