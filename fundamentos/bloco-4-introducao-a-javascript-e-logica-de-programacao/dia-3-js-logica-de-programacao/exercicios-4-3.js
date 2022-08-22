0684df66b171b43cc8c5e753abf05af33c610466
Artur <arturvl1@hotmail.com>
2022-08-22 20:42:16 -0300

    tentativa em alguns exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-js-logica-de-programacao/exercicios-4-3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-js-logica-de-programacao/exercicios-4-3.js
@@ -17,22 +17,37 @@
 // console.log(aux); 
 
 
-// retornar maior e menor palavra do array 
+// // retornar maior e menor palavra do array 
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let menorString = array[0];
-let maiorString = array[0];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let menorString = array[0];
+// let maiorString = array[0];
 
-for (let i = 0; i < array.length; i+=1){
-    if(menorString.length>array[i].length){
-        menorString = array[i];
+// for (let i = 0; i < array.length; i+=1){
+//     if(menorString.length>array[i].length){
+//         menorString = array[i];
+//     }
+// }
+// console.log (menorString);
+
+// for ( let i = 0; i<array.length; i+=1){
+//     if(maiorString.length<array[i].length){
+//         maiorString = array[i];
+//     }
+// }
+// console.log(maiorString);
+
+// exercicio 4 numero primo
+
+// let armazenaPrimos = 0;
+for(let numerador = 2; numerador<=50; numerador += 1){
+    for( let divisor = 2; divisor <= 50; divisor += 1){
+        if(numerador%divisor === 0 && numerador%2 === 1){
+            if (numerador === divisor){
+                console.log(numerador);
+            }
+        }
+      
     }
+    
 }
-console.log (menorString);
-
-for ( let i = 0; i<array.length; i+=1){
-    if(maiorString.length<array[i].length){
-        maiorString = array[i];
-    }
-}
-console.log(maiorString);
