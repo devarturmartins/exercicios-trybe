9c24b459114574817581c9e6cf8535546ebba964
devarturmartins <arturvl1@hotmail.com>
2022-09-17 17:50:25 -0300

    terminado os exercicios do dia 7.1

--- fundamentos/section-7-dia-1/parte2.js
+++ fundamentos/section-7-dia-1/parte2.js
@@ -6,8 +6,8 @@ const fatorial = number => {
     }
     return(result);
 }
-
 console.log(`Esse é o resultado do fatorial: ${fatorial(number)}`);
+
 let string = 'Antônio foi ao banheiro e não sabemos o que aconteceu';
 const maiorPalavra = string => {
     let arrayDaString = string.split(" ");
@@ -16,9 +16,31 @@ const maiorPalavra = string => {
     for(let index = 0; index<arrayDaString.length; index += 1) {
         if(arrayDaString[index].length > maxLength) {
             maxLength = arrayDaString[index].length;
-            result = arrayDaString;
+            result = arrayDaString[index]
         }
     }
     return result;
 }
 console.log(maiorPalavra(string));
+
+// exercicio 4
+
+const substituaX = (nome) => {
+    const frase = 'Tryber x aqui!';
+    let resultado = frase.replace('x', nome);
+    return resultado;
+
+}
+console.log(substituaX('Bebeto'));
+
+const minhasSkills = func => {
+    const skills = ['CSS', 'HTML', 'JavaScript'];
+    let resultado = `${func} 
+    Minhas trës principais habilidades sao: `
+    skills.forEach((skill) => {
+    resultado = `${resultado}
+    - ${skill}`; });
+    return resultado;
+}
+
+console.log(minhasSkills(substituaX('Bebeto')));
