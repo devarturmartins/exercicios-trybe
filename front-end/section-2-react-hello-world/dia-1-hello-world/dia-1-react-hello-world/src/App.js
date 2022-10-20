717c23cec6383814c77d68a233ef27acdbe5787d
devarturmartins <arturvl1@hotmail.com>
2022-10-20 15:46:16 -0300

    fixando 2.1

---
+++ front-end/section-2-react-hello-world/dia-1-hello-world/dia-1-react-hello-world/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+import About from './About';
+
+class App extends Component {
+  render() {
+    return (
+    <About />
+    )
+  }
+}
+
+export default App;
